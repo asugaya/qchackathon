384d17aaa012354f20d8aa0cc850afd624bb824c
U-David-APX\David <David@David-APX.(none)>
2012-06-26 09:31:50 -0700

    Adding Speakers knowledge

---
+++ speakersknowledge/src/com/apxlabs/speakersknowledge/SpeakersKnowledgeActivity.java
@@ -0,0 +1,13 @@
+package com.apxlabs.speakersknowledge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeakersKnowledgeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
